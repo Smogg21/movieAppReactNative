68187ba07131f05f56997f75b6aa90a6f4d41469
Smogg21 <spazzaglia98@gmail.com>
2024-08-08 12:34:36 -0500

    Cambiar tipado de navegación

--- src/presentation/navigation/Navigation.tsx
+++ src/presentation/navigation/Navigation.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 import {HomeScreen} from '../screens/home/HomeScreen';
 import {DetailsScreen} from '../screens/details/DetailsScreen';
 
-const Stack = createStackNavigator();
+export type RootStackParams = {
+  Home: undefined;
+  Details: {movieId: number};
+};
+
+const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
