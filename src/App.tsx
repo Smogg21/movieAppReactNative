decc48ac66ec4f6e71a1f045d517d7aac1572166
Smogg21 <spazzaglia98@gmail.com>
2024-08-08 12:32:08 -0500

    Implemented navigation on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import '../gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {Text, View} from 'react-native';
 
 export const App = () => {
   return (
-    <View>
-      <Text>MoviesApp</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>MoviesApp</Text>
+      </View>
+    </NavigationContainer>
   );
 };
