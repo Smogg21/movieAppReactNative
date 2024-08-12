28dbc3d02892cd7714c7c8173292362c25a72564
Smogg21 <spazzaglia98@gmail.com>
2024-08-12 16:29:46 -0500

    Modified poster styles

--- src/presentation/components/movies/MoviePoster.tsx
+++ src/presentation/components/movies/MoviePoster.tsx
@@ -19,9 +19,9 @@ export const MoviePoster = ({movie, height = 420, width = 300}: Props) => {
       style={({pressed}) => ({
         width,
         height,
-        marginHorizontal: 10,
+        marginHorizontal: 4,
         paddingBottom: 20,
-        paddingHorizontal: 10,
+        paddingHorizontal: 7,
         opacity: pressed ? 0.9 : 1,
       })}>
       <View style={styles.imageContainer}>
