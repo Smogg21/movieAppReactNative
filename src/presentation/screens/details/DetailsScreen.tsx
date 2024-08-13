42cea9fef27a69ed0e11c6da538cd6002fcb7886
Smogg21 <spazzaglia98@gmail.com>
2024-08-13 12:09:53 -0500

    Implemented movie details and useMovie

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,10 +1,28 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Text, View} from 'react-native';
+import {RootStackParams} from '../../navigation/Navigation';
+import {useMovie} from '../../hooks/useMovie';
+import {MovieHeader} from '../../components/movie/MovieHeadet';
 
-export const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailsScreen = ({route}: Props) => {
+  const {movieId} = route.params;
+  // const {movieId} = useRoute().params;
+  const {isLoading, movie} = useMovie(movieId);
+
+  if (isLoading) {
+    return <Text>Loading</Text>;
+  }
+
   return (
     <View>
-      <Text>Details Screen</Text>
+      <MovieHeader
+        originalTitle={movie!.originalTitle}
+        title={movie!.title}
+        poster={movie!.poster}
+      />
     </View>
   );
 };
